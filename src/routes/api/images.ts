6c45bcabf87e24d76a2fcb7510da595763557044
efernandezr <efernandezr@gmail.com>
2021-11-10 17:40:17 +0100

    resizing ok without displaying image

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import imageResize from '../../utilities/imageResize';
+import config from '../../config';
 const images = express.Router();
 
-images.get('/', (req, res) => {
-  res.send('Hello, world images!');
+images.get('/', async (req, res) => {
+  // res.send('Hello, world images!');
+  const fileName = req.query.filename;
+  const width = parseInt(req.query.width as string);
+  const height = parseInt(req.query.height as string);
+
+  imageResize(`${fileName}`, width, height);
 });
 
-imageResize('encenadaport', 200, 200);
+// imageResize('encenadaport', 200, 200);
 
 export default images;
