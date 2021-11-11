2bd214d968bea0cdb081cfc75ebe234d27b900a9
efernandezr <efernandezr@gmail.com>
2021-11-11 19:53:56 +0100

    url params confirmation added- no middleware

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -9,14 +9,33 @@ images.get('/', async (req, res) => {
   const fileName = req.query.filename;
   const width = parseInt(req.query.width as string);
   const height = parseInt(req.query.height as string);
-  const outputImagePath = `${config.ASSETS_PATH}/thumb/${fileName}${width}X${height}_thumb.jpeg`;
-  const fileExist = await imgExist(outputImagePath);
+  console.log(width);
+  console.log(height);
+  console.log(!isNaN(width));
+  console.log(!isNaN(height));
 
-  if (!fileExist) {
-    await imageResize(`${fileName}`, width, height);
-    res.status(200).sendFile(outputImagePath);
+  if (fileName !== undefined && !isNaN(width) && !isNaN(height)) {
+    try {
+      const fileName = req.query.filename;
+      const width = parseInt(req.query.width as string);
+
+      const height = parseInt(req.query.height as string);
+      const outputImagePath = `${config.ASSETS_PATH}/thumb/${fileName}${width}X${height}_thumb.jpeg`;
+      const fileExist = await imgExist(outputImagePath);
+      if (!fileExist) {
+        await imageResize(`${fileName}`, width, height);
+        res.status(200).sendFile(outputImagePath);
+      } else {
+        res.status(200).sendFile(outputImagePath);
+      }
+    } catch (err) {
+      console.log(err);
+      res
+        .status(200)
+        .sendFile('error while processing image path, please try again');
+    }
   } else {
-    res.status(200).sendFile(outputImagePath);
+    res.status(200).send('please use the correct image parameters');
   }
 });
 
