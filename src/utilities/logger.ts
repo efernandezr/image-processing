782eb07120bbf47d2ef02f4b6451e59d50a2c04e
efernandezr <efernandezr@gmail.com>
2021-11-12 16:51:56 +0100

    logger improved - adds logs when exists error in params

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -1,14 +1,19 @@
 import express from 'express';
+import paramsChecker from './paramsChecker';
 
 const logger = (
   req: express.Request,
   res: express.Response,
   next: Function
 ): void => {
-  const fileName = req.query.filename;
-  const width = parseInt(req.query.width as string);
-  const height = parseInt(req.query.height as string);
-  console.log(`${fileName} image processed -> size:${width}x${height} px.`);
+  if (paramsChecker(req)) {
+    const fileName = req.query.filename;
+    const width = parseInt(req.query.width as string);
+    const height = parseInt(req.query.height as string);
+    console.log(`${fileName} image processed -> size:${width}x${height} px.`);
+  } else {
+    console.log('couldnt process any image, correct params not present in url');
+  }
   next();
 };
 
